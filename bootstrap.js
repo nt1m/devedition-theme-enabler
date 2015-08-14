b9cf4d43df4d17f9636b2db946cf5ebfe28eb8b0
Tim Nguyen <ntim.bugs@gmail.com>
2015-08-14 21:27:46 +0200

    First commit

---
+++ bootstrap.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
+
+const {Services} = Cu.import("resource://gre/modules/Services.jsm");
+const {LightweightThemeManager} = Cu.import("resource://gre/modules/LightweightThemeManager.jsm");
+
+function install() {
+  Services.prefs.setCharPref("devtools.theme", "dark");
+  LightweightThemeManager.currentTheme = LightweightThemeManager.getUsedTheme("firefox-devedition@mozilla.org");
+}
+
+function startup() {
+  LightweightThemeManager.addBuiltInTheme({
+    id: "firefox-devedition@mozilla.org",
+    name: "Developer Edition",
+    headerURL: "resource:///chrome/browser/content/browser/defaultthemes/devedition.header.png",
+    iconURL: "resource:///chrome/browser/content/browser/defaultthemes/devedition.icon.png",
+    author: "Mozilla"
+  });
+}
+function shutdown() {
+  LightweightThemeManager.clearBuiltInThemes();
+}
+
+function uninstall() {
+  LightweightThemeManager.currentTheme = null;
+  LightweightThemeManager.clearBuiltInThemes();
+}
